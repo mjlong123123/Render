8df4473156eea3ead30c4d4790fbbdd9948cabeb
mjlong123 <chenjiulong68@gmail.com>
2020-04-26 22:38:58 +0800

    add hint parameters

--- app/src/main/java/com/dragon/render/QRCodeDecoder.kt
+++ app/src/main/java/com/dragon/render/QRCodeDecoder.kt
@@ -7,6 +7,7 @@ import android.os.HandlerThread
 import android.text.TextUtils
 import android.util.Log
 import android.view.Surface
+import com.google.zxing.BarcodeFormat
 import com.google.zxing.BinaryBitmap
 import com.google.zxing.DecodeHintType
 import com.google.zxing.PlanarYUVLuminanceSource
@@ -20,6 +21,8 @@ class QRCodeDecoder(w: Int, h: Int, private val callback: (String) -> Unit) : Im
     private val imageReader: ImageReader
     private val bufferByte: ByteArray
     private var qrCodeResult: String? = null
+    private val qrCodeReader = QRCodeReader()
+    private val hints: Hashtable<DecodeHintType, Any> = Hashtable<DecodeHintType, Any>()
 
     init {
         frameThread.start()
@@ -27,6 +30,8 @@ class QRCodeDecoder(w: Int, h: Int, private val callback: (String) -> Unit) : Im
         imageReader = ImageReader.newInstance(w, h, ImageFormat.YUV_420_888, 2)
         imageReader.setOnImageAvailableListener(this, frameHandler)
         bufferByte = ByteArray(w * h * 2)
+        hints[DecodeHintType.CHARACTER_SET] = "utf-8" // 设置二维码内容的编码
+        hints[DecodeHintType.POSSIBLE_FORMATS] = BarcodeFormat.QR_CODE
         Log.d("QRCodeDecoder", " w:$w h:$h")
     }
 
@@ -47,13 +52,10 @@ class QRCodeDecoder(w: Int, h: Int, private val callback: (String) -> Unit) : Im
                 offset += image.planes[1].buffer.limit()
                 image.planes[2].buffer.get(bufferByte, offset, image.planes[2].buffer.limit())
 
-                val reader = QRCodeReader()
-                val hints: Hashtable<DecodeHintType, String> = Hashtable<DecodeHintType, String>()
-                hints.put(DecodeHintType.CHARACTER_SET, "utf-8") // 设置二维码内容的编码
                 val source = PlanarYUVLuminanceSource(bufferByte, image.width, image.height, 0, 0, image.width, image.height, false)
                 val tempBitmap = BinaryBitmap(HybridBinarizer(source))
                 try {
-                    qrCodeResult = reader.decode(tempBitmap)?.text
+                    qrCodeResult = qrCodeReader.decode(tempBitmap, hints)?.text
                     callback.invoke(qrCodeResult ?: "")
                 } catch (e: Exception) {
                     e.printStackTrace()
